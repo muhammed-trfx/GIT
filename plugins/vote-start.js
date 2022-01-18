63a24bf58ecc11bbb8f4063d44cf02a88e9e8d56
MUHAMMED <93473199+muhammed-userbot@users.noreply.github.com>
2022-01-18 13:32:08 +0530

    ᴡᴀ.ᴍᴇᡃ❦ɪ̂ᴍ.ᴛᴀᴜʀᴜsꫂ⁩࿐

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -2,14 +2,14 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
     if (id in conn.vote) {
-        await conn.sendButton(m.chat, '_There are still votes in this chat!_', '© Eva', 'DELETE VOTE', `${usedPrefix}deletevote`)
+        await conn.sendButton(m.chat, '_There are still votes in this chat!_', '𝚃𝙰𝚄𝚁𝚄𝚂²⁰²²', 'DELETE VOTE', `${usedPrefix}deletevote`)
         throw false
     }
     await conn.send2Button(m.chat, `Voting starts!
 *${usedPrefix}upvote* - for sure
 *${usedPrefix}devote* - for not
 *${usedPrefix}checkvote* - to check the vote
-*${usedPrefix}deletevote* - to delete votes`, '© Eva', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`)
+*${usedPrefix}deletevote* - to delete votes`, '𝚃𝙰𝚄𝚁𝚄𝚂²⁰²²', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`)
     conn.vote[id] = [
         text,
         [],
